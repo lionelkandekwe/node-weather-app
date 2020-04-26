8c0eb68cad87fe8c8e7272531e65c2b4223cab2b
Lionel Kandekwe <liokandekwe@gmail.com>
2020-04-26 02:19:16 +0200

    Added The  humidity

--- utilis/forecast.js
+++ utilis/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('unable to find location', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out.There is a ' + body.current.feelslike + '% chance of rain')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out.It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
 
     })
